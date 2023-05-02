775acb9760bbf80e1d8da61864d905de2dffd626
Rukin Danil <rukin2018d@gmail.com>
2023-05-02 21:31:54 +0700

    init

---
+++ Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using WpfMvvmTemplate.Infrastructure.Commands.Base;
+
+namespace WpfMvvmTemplate.Infrastructure.Commands
+{
+    public class CloseWindowCommand : Command
+    {
+        protected override bool CanExecute(object parameter) => (parameter as Window ?? App.FocusedWindow ?? App.ActivedWindow) != null;
+
+        protected override void Execute(object parameter) => (parameter as Window ?? App.FocusedWindow ?? App.ActivedWindow)?.Close();
+    }
+}
